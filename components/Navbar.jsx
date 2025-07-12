a1a75d62244bf12336e8aba5da8faf1d9cabe60d
gptfleet <gptfleet@gmail.com>
2025-07-12 21:52:41 +0000

    Update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,6 @@ const navLinks = [
   { name: 'Home', href: '/' },
   {
     name: 'Welding Courses',
-    href: '/welding/master',
     children: [
       { name: 'Master', href: '/welding/master' },
       { name: 'Hobby', href: '/welding/hobby' },
@@ -12,7 +11,6 @@ const navLinks = [
   },
   {
     name: 'Admissions',
-    href: '/admissions/choosing',
     children: [
       { name: 'Choosing', href: '/admissions/choosing' },
       { name: 'Information', href: '/admissions/information' },
@@ -21,7 +19,6 @@ const navLinks = [
   },
   {
     name: 'Payment',
-    href: '/payment/financing',
     children: [
       { name: 'Financing', href: '/payment/financing' },
       { name: 'Store', href: '/payment/store' },
@@ -31,7 +28,6 @@ const navLinks = [
   { name: 'Reviews', href: '/reviews' },
   {
     name: 'About',
-    href: '/about/blog',
     children: [
       { name: 'Blog', href: '/about/blog' },
       { name: 'FAQ', href: '/about/faq' },
@@ -50,14 +46,26 @@ export default function Navbar() {
             key={link.name}
             className={`nav-item ${link.children ? 'dropdown relative group' : ''}`}
           >
-            <Link href={link.href} className="hover:text-blue-600 font-medium">
-              {link.name}
-            </Link>
+            {link.children ? (
+              <button
+                type="button"
+                className="font-medium hover:text-blue-600"
+              >
+                {link.name}
+              </button>
+            ) : (
+              <Link
+                href={link.href}
+                className="font-medium hover:text-blue-600"
+              >
+                {link.name}
+              </Link>
+            )}
 
             {link.children && (
               <ul className="absolute left-0 mt-2 hidden group-hover:block bg-white border rounded shadow-md">
                 {link.children.map((child) => (
-                  <li key={child.name} className="px-4 py-2 hover:bg-gray-100">
+                  <li key={child.name} className="px-4 py-2 hover:bg-gray-100 whitespace-nowrap">
                     <Link href={child.href}>{child.name}</Link>
                   </li>
                 ))}
