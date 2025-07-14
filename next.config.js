5f29c73a421a4007416e7da9f8806d6ead841cbe
gptfleet <gptfleet@gmail.com>
2025-07-14 23:14:05 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
     allowedDevOrigins: [
       "https://3000-firebase-tradeschoolwebsite-1752350821054.cluster-vyr53kd25jc2yvngldrwyq6zc4.cloudworkstations.dev",
     ],
-    images: {
- unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  },
+};
