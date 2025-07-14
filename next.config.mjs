3bee7485f1b43f93e487d96a1752be38c8cc5550
gptfleet <gptfleet@gmail.com>
2025-07-14 21:55:02 +0000

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@
 const nextConfig = {
     output: 'export',
     allowedDevOrigins: [
-      "https://3000-firebase-tradeschoolwebsite-1752350821054.cluster-vyr53kd25jc2yvngldrwyq6zc4.cloudworkstations.dev"
-    ]
+      "https://3000-firebase-tradeschoolwebsite-1752350821054.cluster-vyr53kd25jc2yvngldrwyq6zc4.cloudworkstations.dev",
+    ],
+    images: {
+ unoptimized: true,
+    },
   };
   
   export default nextConfig;
