4d5679197dbc95e6c3f107ccb7d11b43d099ced1
gptfleet <gptfleet@gmail.com>
2025-07-15 22:46:05 +0000

    Update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,9 +6,11 @@ export default function Layout({ children, pageTitle }) {
     <div className="min-h-screen flex flex-col">
       <Header pageTitle={pageTitle} />
       <main className="flex-1 pt-[112px] pb-8 bg-white">
-        <div className="mx-auto px-4 w-full max-w-2xl prose prose-neutral">
-          {children}
-        </div>
+        {/* 
+          We don't constrain children here directly,
+          so you have full flexibility on each page 
+        */}
+        {children}
       </main>
       <Footer />
     </div>
