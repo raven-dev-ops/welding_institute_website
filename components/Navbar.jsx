36d134963384f1d7dfaf68bcb21a375b0b6c4288
gptfleet <gptfleet@gmail.com>
2025-07-15 02:23:14 +0000

    Update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
 
         {/* Nav Links Bar */}
         <div className="w-full bg-white">
-          <ul className="flex flex-wrap justify-center gap-x-[80px] gap-y-4 py-6">
+          <ul className="flex flex-wrap justify-center gap-x-[80px] gap-y-4 py-6 list-none">
             {navLinks.map((link) => {
               const isDropdown = link.children && link.children.length > 0;
 
@@ -65,26 +65,24 @@ export default function Navbar() {
                     className="font-medium text-ciwt-dark hover:text-ciwt-blue transition-colors duration-200 no-underline flex items-center"
                   >
                     {link.name}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className={`ml-1 h-4 w-4 transform transition-transform duration-200 ${
-                        isDropdown
-                          ? "group-hover:rotate-0 rotate-180"
-                          : "rotate-180"
-                      }`}
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.21 8.27a.75.75 0 01.02-1.06z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    {isDropdown && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="ml-1 h-4 w-4 transform transition-transform duration-200 rotate-180 group-hover:rotate-0"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.21 8.27a.75.75 0 01.02-1.06z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    )}
                   </Link>
 
                   {isDropdown && (
-                    <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 hidden group-hover:block bg-white py-4 rounded shadow-lg z-50">
+                    <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 hidden group-hover:block bg-white py-4 rounded shadow-lg z-50 list-none">
                       {link.children.map((child) => (
                         <li
                           key={child.name}
